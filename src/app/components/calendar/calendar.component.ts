378ffb519660413b9b55e95d124cfe3509745158
Alberto Moreno <alberto.moreno2357@gmail.com>
2022-12-14 22:50:14 +0100

    eventsAdded

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -13,12 +13,22 @@ import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 export class CalendarViewComponent implements OnInit {
 
   calendarOptions: CalendarOptions = {
-    plugins: [dayGridPlugin, timeGridPlugin, bootstrap5Plugin],
+    plugins: [dayGridPlugin, timeGridPlugin, bootstrap5Plugin, interactionPlugin],
     initialView: 'dayGridMonth',
+    dateClick: this.handleDateClick.bind(this), // MUST ensure `this` context is maintained
+    events: [
+      { title: 'Cumple mami', date: '2022-12-05' },
+      { title: 'Navidad', date: '2022-12-24' }
+    ]
   };
 
   constructor() { }
 
   ngOnInit() {}
+
+  handleDateClick(arg) {
+    alert('date click! ' + arg.dateStr);
+  }
+
 
 }
