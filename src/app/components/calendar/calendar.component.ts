56210aab31179049f5ba467dc17ec7bad3682e20
Alberto Moreno <alberto.moreno2357@gmail.com>
2022-12-16 15:37:10 +0100

    EventTitleShow

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -4,6 +4,7 @@ import interactionPlugin from '@fullcalendar/interaction';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+import momentPlugin from '@fullcalendar/moment';
 
 @Component({
   selector: 'app-calendar',
@@ -12,23 +13,36 @@ import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 })
 export class CalendarViewComponent implements OnInit {
 
+  presentDays = 0;
+  absentDays = 0;
+  events: any = [
+    { title: 'Present', date: '2022-12-05', color: 'red' },
+    { title: 'Absent', date: '2022-12-24' }
+  ];
+
   calendarOptions: CalendarOptions = {
-    plugins: [dayGridPlugin, timeGridPlugin, bootstrap5Plugin, interactionPlugin],
+    plugins: [dayGridPlugin, timeGridPlugin, bootstrap5Plugin, interactionPlugin, momentPlugin],
     initialView: 'dayGridMonth',
-    dateClick: this.handleDateClick.bind(this), // MUST ensure `this` context is maintained
-    events: [
-      { title: 'Cumple mami', date: '2022-12-05' },
-      { title: 'Navidad', date: '2022-12-24' }
-    ]
+    eventClick: this.handleDateClick.bind(this),// MUST ensure `this` context is maintained
+    events: this.events
   };
 
   constructor() { }
 
-  ngOnInit() {}
-
-  handleDateClick(arg) {
-    alert('date click! ' + arg.dateStr);
+  ngOnInit() {
+    this.events.forEach((e: {[x: string]: string }) => {
+      if(e.title === 'Present'){
+        this.presentDays++;
+      }else{
+        this.absentDays++;
+      }
+    });
+    console.log(this.presentDays);
+    console.log(this.absentDays);
   }
 
+  handleDateClick(arg) {
+    console.log(arg.event.title);
+  }
 
 }
