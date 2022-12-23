43ec3f7c66d18f3e314b5f5064681e17b37c8464
Alberto Moreno <alberto.moreno2357@gmail.com>
2022-12-23 19:51:53 +0100

    adv

--- src/app/components/documents/documents.component.ts
+++ src/app/components/documents/documents.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { getStorage, ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
+
 
 @Component({
   selector: 'app-documents',
@@ -9,6 +11,41 @@ export class DocumentsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getImages();
+  }
+
+  uploadFile($event: any) {
+
+    const file = $event.target.files[0];
+    console.log(file);
+    const storage = getStorage();
+    const imgRef = ref(storage, `images/${file.name}`);
+
+
+    uploadBytes(imgRef, file).then((snapshot) => {
+      console.log('Uploaded a blob or file!');
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  getImages(){
+    const storage = getStorage();
+    const imgRef = ref(storage, `images`);
+
+    listAll(imgRef)
+    .then(async response => {
+
+      for(const item of response.items){
+        const url = await getDownloadURL(item);
+        console.log(url);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  }
 
 }
